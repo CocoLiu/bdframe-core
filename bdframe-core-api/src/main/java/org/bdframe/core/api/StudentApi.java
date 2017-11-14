b8d03c375ecdba816a1f8018c9c8558abb512788
CocoLiu <CocoLiu@DESKTOP-COCO>
2017-11-14 10:43:44 +0800

    add author

--- bdframe-core-api/src/main/java/org/bdframe/core/api/StudentApi.java
+++ bdframe-core-api/src/main/java/org/bdframe/core/api/StudentApi.java
@@ -3,7 +3,10 @@ package org.bdframe.core.api;
 import java.util.List;
 
 import org.bdframe.core.api.dto.StudentDTO;
-
+/**
+ * @author CocoLiu 2017
+ * @version $Id AccountInfoApi.java$
+ */
 public interface StudentApi {
     List<StudentDTO> listStudents();
 }
